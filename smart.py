3364cb1a7b0ad65bac596b488901f92fa2f321a1
Saudi-bot755 <alrshydymhmdhzam8@gmail.com>
2025-07-14 23:36:07 +0300

    Update smart.py

--- smart.py
+++ smart.py
@@ -113,4 +113,5 @@ def send_whatsapp(to, body):
     return jsonify({"status": "sent"})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", 
+    port=int(os.environ.get("PORT", 10000)))
