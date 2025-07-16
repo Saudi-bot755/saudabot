49d1d27fc26c73b46c8cb2b82183afa551c6f8d0
Saudi-bot755 <alrshydymhmdhzam8@gmail.com>
2025-07-16 16:51:18 +0300

    Update smart.py

--- smart.py
+++ smart.py
@@ -224,5 +224,6 @@ def message():
             answer = "عذراً، حدث خطأ أثناء معالجة الرسالة."
         return respond(answer)
 
-if __name__ == '__main__':
-    app.run()
+    if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
