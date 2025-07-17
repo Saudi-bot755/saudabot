c792690632fa525c705cceb3b9606fc59c28761a
Saudi-bot755 <alrshydymhmdhzam8@gmail.com>
2025-07-17 19:28:10 +0300

    Update smart.py

--- smart.py
+++ smart.py
@@ -120,4 +120,4 @@ def upload_img(image_path):
     return None
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host='0.0.0.0', port=8080)
